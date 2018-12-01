08b50fd414ceaab6b0ce82e3e7d62fd3f33c71c8
Vinicius Garcia <viniciusgarcias@hotmail.com>
2018-12-01 16:10:48 -0200

    Independente v1.5

--- main.c
+++ main.c
@@ -5,8 +5,9 @@ sbit type = P3^5;
 
 void delay(int delay_time);
 void begin();
-void start(int );
+void start();
 void send_signal();
+void clear_signal();
 void finish(int delay_time);
 void allOn();
 void allOff();
@@ -16,14 +17,18 @@ void hitAndTurn(int delay_time, int repeat);
 void runAndBack(int delay_time, int repeat);
 void wait(int delay_time);
 void set(float value, int delay_time);
-
+void load(int delay_time);
+void unload(int delay_time);
+void reverseLoad(int delay_time);
+void loadReverseLoad(int delay_time, repeat);
+void loadUnload(int delay_time, repeat);
 
 int main( ) {
-	begin();
 	while(1) {
-		run(10, 6);
-		hitAndTurn(10, 3);
-		runAndBack(10, 2);
+		run(50,10);
+		loadReverseLoad(150,2);
+		run(100,5);
+		loadReverseLoad(50,10);
 	}
 }
 
@@ -34,110 +39,65 @@ void wait(int delay_time) {
 
 void blink(int delay_time, int repeat) {
 	int i;
-	start(0);
+	start();
 	for(i=0 ; i<repeat ; i++) {
 		allOn();
 		delay(delay_time);
 		allOff();
 		delay(delay_time);
 	}
-	finish(0);
 }
 
 void hitAndTurn(int delay_time, int repeat) {
 	int i;
-	start(0);
+	start();
 	if (type==0) {
 		for(i=0 ; i<repeat ; i++) {
-			P1 = 0xF3; // 11110011
-			delay(delay_time);
-			P1 = 0xF9; // 11111001
-			delay(delay_time);
-			P1 = 0xFC; // 11111100
-			delay(delay_time);
-			P1 = 0x7E; // 01111110
-			delay(delay_time);
-			P1 = 0x3F; // 00111111
-			delay(delay_time);
-			P1 = 0x9F; // 10011111
-			delay(delay_time);
-			P1 = 0xCF; // 11001111
-			delay(delay_time);
-			P1 = 0x9F; // 10011111
-			delay(delay_time);
-			P1 = 0x3F; // 00111111
-			delay(delay_time);
-			P1 = 0x7E; // 01111110
-			delay(delay_time);
-			P1 = 0xFC; // 11111100
-			delay(delay_time);
-			P1 = 0xF9; // 11111001
-			delay(delay_time);
+			set(0xF3, delay_time); // 11110011
+			set(0xF9, delay_time); // 11111001
+			set(0xFC, delay_time); // 11111100
+			set(0x7E, delay_time); // 01111110
+			set(0x3F, delay_time); // 00111111
+			set(0x9F, delay_time); // 10011111
+			set(0xCF, delay_time); // 11001111
+			set(0x9F, delay_time); // 10011111
+			set(0x3F, delay_time); // 00111111
+			set(0x7E, delay_time); // 01111110
+			set(0xFC, delay_time); // 11111100
+			set(0xF9, delay_time); // 11111001
 		}
 	}
 	else if(type==1) {
 		for(i=0 ; i<repeat ; i++) {
-			P1 = 0xCF; // 11001111
-			delay(delay_time);
-			P1 = 0x9F; // 10011111
-			delay(delay_time);
-			P1 = 0x3F; // 00111111
-			delay(delay_time);
-			P1 = 0x7E; // 01111110
-			delay(delay_time);
-			P1 = 0xFC; // 11111100
-			delay(delay_time);
-			P1 = 0xF9; // 11111001
-			delay(delay_time);
-			P1 = 0xF3; // 11110011
-			delay(delay_time);
-			P1 = 0xF9; // 11111001
-			delay(delay_time);
-			P1 = 0xFC; // 11111100
-			delay(delay_time);
-			P1 = 0x7E; // 01111110
-			delay(delay_time);
-			P1 = 0x3F; // 00111111
-			delay(delay_time);
-			P1 = 0x9F; // 10011111
-			delay(delay_time);
+			set(0xCF, delay_time); // 11001111
+			set(0x9F, delay_time); // 10011111
+			set(0x3F, delay_time); // 00111111
+			set(0x7E, delay_time); // 01111110
+			set(0xFC, delay_time); // 11111100
+			set(0xF9, delay_time); // 11111001
+			set(0xF3, delay_time); // 11110011
+			set(0xF9, delay_time); // 11111001
+			set(0xFC, delay_time); // 11111100
+			set(0x7E, delay_time); // 01111110
+			set(0x3F, delay_time); // 00111111
+			set(0x9F, delay_time); // 10011111
 		}
 	}
-	finish(0);
 }
 
 void run(int delay_time, int repeat) {
 	int i;
-	start(0);
+	start();
 	for (i=0 ; i<repeat ; i++) {
-		set(0xF3, delay_time);
-		set(0xF9, delay_time);
-		set(0xFC, delay_time);
-		set(0x7E, delay_time);
-		set(0x3F, delay_time);
-		set(0x9F, delay_time);
-		set(0xCF, delay_time);
-		set(0xE7, delay_time);
-		/*
-		P1 = 0xF3; // 11110011
-		delay(delay_time);
-		P1 = 0xF9; // 11111001
-		delay(delay_time);
-		P1 = 0xFC; // 11111100
-		delay(delay_time);
-		P1 = 0x7E; // 01111110
-		delay(delay_time);
-		P1 = 0x3F; // 00111111
-		delay(delay_time);
-		P1 = 0x9F; // 10011111
-		delay(delay_time);
-		P1 = 0xCF; // 11001111
-		delay(delay_time);
-		P1 = 0xE7; // 11100111
-		delay(delay_time);
-		*/
+		set(0xF3, delay_time); // 11110011
+		set(0xF9, delay_time); // 11111001
+		set(0xFC, delay_time); // 11111100
+		set(0x7E, delay_time); // 01111110
+		set(0x3F, delay_time); // 00111111
+		set(0x9F, delay_time); // 10011111
+		set(0xCF, delay_time); // 11001111
+		set(0xE7, delay_time); // 11100111
 	}
-	finish(0);
 }
 
 void set(float value, int delay_time) {
@@ -147,92 +107,87 @@ void set(float value, int delay_time) {
 
 void runAndBack(int delay_time, int repeat) {
 	int i;
-	start(0);
+	start();
 	for (i=0 ; i<repeat ; i++) {
-		P1 = 0xF3; // 11110011
-		delay(delay_time);
-		P1 = 0xF9; // 11111001
-		delay(delay_time);
-		P1 = 0xF3; // 11110011
-		delay(delay_time);
-		P1 = 0xF9; // 11111001
-		delay(delay_time);
-		P1 = 0xFC; // 11111100
-		delay(delay_time);
-		P1 = 0xF9; // 11111001
-		delay(delay_time);
-		P1 = 0xFC; // 11111100
-		delay(delay_time);
-		P1 = 0x7E; // 01111110
-		delay(delay_time);
-		P1 = 0xFC; // 11111100
-		delay(delay_time);
-		P1 = 0x7E; // 01111110
-		delay(delay_time);
-		P1 = 0x3F; // 00111111
-		delay(delay_time);
-		P1 = 0x7E; // 01111110
-		delay(delay_time);
-		P1 = 0x3F; // 00111111
-		delay(delay_time);
-		P1 = 0x9F; // 10011111
-		delay(delay_time);
-		P1 = 0x3F; // 00111111
-		delay(delay_time);
-		P1 = 0x9F; // 10011111
-		delay(delay_time);
-		P1 = 0xCF; // 11001111
-		delay(delay_time);
-		P1 = 0x9F; // 10011111
-		delay(delay_time);
-		P1 = 0xCF; // 11001111
-		delay(delay_time);
-		P1 = 0xE7; // 11100111
-		delay(delay_time);
-		P1 = 0xCF; // 11001111
-		delay(delay_time);
-		P1 = 0xE7; // 11100111
-		delay(delay_time);
-		P1 = 0xF3; // 11110011
-		delay(delay_time);
-		P1 = 0xE7; // 11100111
-		delay(delay_time);
+		set(0xF3, delay_time); // 11110011
+		set(0xF9, delay_time); // 11111001
+		set(0xF3, delay_time); // 11110011
+		set(0xF9, delay_time); // 11111001
+		set(0xFC, delay_time); // 11111100
+		set(0xF9, delay_time); // 11111001
+		set(0xFC, delay_time); // 11111100
+		set(0x7E, delay_time); // 01111110
+		set(0xFC, delay_time); // 11111100
+		set(0x7E, delay_time); // 01111110
+		set(0x3F, delay_time); // 00111111
+		set(0x7E, delay_time); // 01111110
+		set(0x3F, delay_time); // 00111111
+		set(0x9F, delay_time); // 10011111
+		set(0x3F, delay_time); // 00111111
+		set(0x9F, delay_time); // 10011111
+		set(0xCF, delay_time); // 11001111
+		set(0x9F, delay_time); // 10011111
+		set(0xCF, delay_time); // 11001111
+		set(0xE7, delay_time); // 11100111
+		set(0xCF, delay_time); // 11001111
+		set(0xE7, delay_time); // 11100111
 	}
-	finish(0);
 }
 
-void load(int delay_time, int repeat) {
-	P1 = 0xF7; // 11110111
-	P1 = 0xF3; // 11110011
-	P1 = 0xF1; // 11110001
-	P1 = 0xF0; // 11110000
-	P1 = 0x70; // 01110000
-	P1 = 0x30; // 00110000
-	P1 = 0x10; // 00010000
-	P1 = 0x00; // 00000000
+void loadUnload(int delay_time, repeat){
+	int i;
+	start();
+	for ( i=0 ; i<repeat ; i++) {
+		load(delay_time);
+		unload(delay_time);
+	}
 }
 
-void unload(int delay_time, int repeat) {
-	P1 = 0x10; // 00010000
-	P1 = 0x30; // 00110000
-	P1 = 0x70; // 01110000
-	P1 = 0xF0; // 11110000
-	P1 = 0xF1; // 11110001
-	P1 = 0xF3; // 11110011
-	P1 = 0xF7; // 11110111
-	P1 = 0xFF; // 11111111
+void loadReverseLoad(int delay_time, repeat) {
+	int i;
+	start();
+	for ( i=0 ; i<repeat ; i++) {
+		load(delay_time);
+		reverseLoad(delay_time);
+	}
 }
 
-void reverseLoad(int delay_time, int repeat) {
-	P1 = 0x08; // 00001000
-	P1 = 0x0C; // 00001100
-	P1 = 0x0E; // 00001110
-	P1 = 0x0F; // 00001111
+void load(int delay_time) {
+	set(0xF7, delay_time); // 11110111
+	set(0xF3, delay_time); // 11110011
+	set(0xF1, delay_time); // 11110001
+	set(0xF0, delay_time); // 11110000
+	set(0x70, delay_time); // 01110000
+	set(0x30, delay_time); // 00110000
+	set(0x10, delay_time); // 00010000
+	set(0x00, delay_time); // 00000000
+}
+
+void unload(int delay_time) {
+	set(0x10, delay_time); // 00010000
+	set(0x30, delay_time); // 00110000
+	set(0x70, delay_time); // 01110000
+	set(0xF0, delay_time); // 11110000
+	set(0xF1, delay_time); // 11110001
+	set(0xF3, delay_time); // 11110011
+	set(0xF7, delay_time); // 11110111
+	set(0xFF, delay_time); // 11111111
+}
+
+void reverseLoad(int delay_time) {
+	set(0x08, delay_time); // 00001000
+	set(0x0C, delay_time); // 00001100
+	set(0x0E, delay_time); // 00001110
+	set(0x0F, delay_time); // 00001111
+	set(0x8F, delay_time); // 10001111
+	set(0xCF, delay_time); // 11001111
+	set(0xEF, delay_time); // 11101111
+	set(0xFF, delay_time); // 11111111
 }
 	
 void delay(int value) { 
 	int i,j;
-	for( i=0 ; i<1000 ; i++)
+	for( i=0 ; i<100 ; i++)
 		for( j=0 ; j<value ; j++ );
 	return;
 }
@@ -248,7 +203,10 @@ void allOff() {
 
 void send_signal() {
 	signal=1;
-	delay(1);
+	return;
+}
+
+void clear_signal() {
 	signal=0;
 	return;
 }
@@ -260,15 +218,19 @@ void begin() {
 	return;
 }
 
-void start(int delay_time) {
-	if(type == 0){
-		while(signal==0);
+void start() {
+	/*if(type == 0){
+		delay(50);
+		while(signal==0) {
+		};
 	}	
-	else if(type == 1) {
-		send_signal();
-		delay(delay_time);
+	else {
+		clear_signal();
+		delay(100);
+		send_signal(); 
 	}
 	return;
+	*/
 }
 
 void finish(int delay_time){
